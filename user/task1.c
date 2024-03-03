3b2525b944acbca6ca7d019a64277cd3eab4ce48
mozhaa <mozhay2005@gmail.com>
2024-03-03 20:29:40 +0300

    Task 1 complete

--- user/task1.c
+++ user/task1.c
@@ -3,25 +3,36 @@
 #include "user/user.h"
 
 int main(int argc, char** argv) {
+
+    // Usage: task1 [b]
+    // default - variant (a)
+    // [b] - variant (b) is used (kill child process)
+
+    int variant_b = 0;
+    if (argc > 1 && argv[1][0] == 'b') variant_b = 1;
+
     int pid = fork();
     if (pid == 0) {
         // child
-        sleep(10);
+        sleep(50);
         exit(1);
     }
     if (pid < 0) {
         // fork error
-        fprintf(2, "error occured during fork()\n");
+        fprintf(2, "Error occured during fork()\n");
         exit(5);
     }
     
     // parent
     int parent_pid = getpid();
-    printf("parent_pid = %d, child_pid = %d\n", parent_pid, pid);
+    printf("Parent's PID:\t%d\nChild's PID:\t%d\n...\n", parent_pid, pid);
 
-    // (a)
-    int status, wpid;
-    while ((wpid = wait(&status)) > 0);
-    printf("wpid = %d, status = %d\n", wpid, status);
-    exit(0);   
+    if (variant_b) {
+        kill(pid);
+    }
+
+    int status;
+    int wpid = wait(&status);
+    printf("Finished PID:\t%d\nExit status:\t%d\n", wpid, status);
+    exit(0);
 }
